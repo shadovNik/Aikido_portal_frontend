c1613aace52e6d4281bb06ebc76cd7294c0fe8d1
viktorcerepanov <cherepanov0077@gmail.com>
2025-07-01 18:55:15 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Helloworld from "./helloworld.jsx";
+
+function App() {
+  return (
+    <div>
+        <Helloworld></Helloworld>
+    </div>
+  )
+}
+
+export default App
