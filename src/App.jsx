20af9221aab9a58e5211b4d2b8631563b5d445c1
viktorcerepanov <cherepanov0077@gmail.com>
2025-07-03 21:49:02 +0500

    page with clubs for non-authorized. Loading clubs from JSON

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
 import Sidebar from "./react/components/sidebar/Sidebar.jsx";
 import Home from "./react/pages/home/Home.jsx";
+import {Route, Routes} from "react-router-dom";
+import Login from "./react/pages/login/Login.jsx";
+import ListClubs from "./react/pages/list-clubs/ListClubs.jsx";
 
 function App() {
     return (
         <div className="app_div">
             <Sidebar/>
             <main className="main_div">
-                <Home/>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/list-clubs" element={<ListClubs />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
             </main>
         </div>
     )
